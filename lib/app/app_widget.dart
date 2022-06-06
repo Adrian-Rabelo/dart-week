eaac1d087414ecd473abf1ac76c8e69f6834047b
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-06-06 17:28:53 -0400

    criando primeira pagina

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
 
-  const AppWidget({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Job Timer',
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
   }
 }
