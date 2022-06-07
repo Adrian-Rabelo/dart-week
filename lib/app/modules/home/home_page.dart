83058294c67e16fdb0d0a1d3aee5592f06abe351
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-06-07 18:25:49 -0400

    preparando home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:job_timer/app/core/database/database.dart';
-import 'package:job_timer/app/entities/project.dart';
-import 'package:job_timer/app/entities/project_status.dart';
+import 'package:job_timer/app/modules/home/widgets/header_projects_menu.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,19 +7,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      drawer: const Drawer(
+        child: SafeArea(
+            child: ListTile(
+          title: Text('Sair'),
+        )),
       ),
-      body: Column(
-        children: [
-          Container(),
-          ElevatedButton(
-            onPressed: () async {
-              
-            },
-            child: const Text('BOTÃO'),
-          )
-        ],
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              title: const Text('Projetos'),
+              expandedHeight: 100,
+              toolbarHeight: 100,
+              centerTitle: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(15),
+                ),
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: HeaderProjectsMenu(),
+              pinned: true,
+            ),
+            SliverList(
+                delegate: SliverChildListDelegate([
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+              ),
+              Container(
+                color: Colors.blue,
+                height: 200,
+                child: Text('Chegou final'),
+              ),
+            ])),
+          ],
+        ),
       ),
     );
   }
