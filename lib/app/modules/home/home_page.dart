e9fa21a4dd172109bfd41842c5aea1e0ad9b0775
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-06-07 17:43:16 -0400

    testando conexão com banco de dados

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/core/database/database.dart';
+import 'package:job_timer/app/entities/project.dart';
+import 'package:job_timer/app/entities/project_status.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  const HomePage({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(
+            onPressed: () async {
+              
+            },
+            child: const Text('BOTÃO'),
+          )
+        ],
+      ),
+    );
   }
 }
