444799d83514479333f763dfd3d6e74e51ce832b
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2022-06-06 22:40:35 -0400

    fazendo login com google (WIP)

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -2,15 +2,18 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/modules/home/home_module.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
+import 'package:job_timer/app/services/auth/auth_service_impl.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+    Bind.lazySingleton<AuthServiceImpl>((i) => AuthServiceImpl()),
+  ];
 
   @override
   List<ModularRoute> get routes => [
     ChildRoute('/', child: (context, args) => const SplashPage()),
-    ModuleRoute('/login', module: LoginModule()),
-    ModuleRoute('/home', module: HomeModule()),
+    ModuleRoute('/login/', module: LoginModule()),
+    ModuleRoute('/home/', module: HomeModule()),
   ];
 }
